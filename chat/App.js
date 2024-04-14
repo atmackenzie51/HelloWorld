f20eea040fdf6b42636c26930ad7bee2c0693d00
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-04-14 15:56:50 -0400

    implementing the react native navigator

--- chat/App.js
+++ chat/App.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//import the screens
+import Chat from './components/Chat';
+import Start from './components/Start';
+
+//import React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//create the navigator
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.blue}>Hello World!</Text>
-      <Text style={styles.bigRed}>How are you?</Text>
-      <Text style={[styles.blue, styles.bigRed]}>I am feeling blue...</Text>
-      <View style={styles.box}>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.blue}>Hello World!</Text>
+        <Text style={styles.bigRed}>How are you?</Text>
+        <Text style={[styles.blue, styles.bigRed]}>I am feeling blue...</Text>
+        <View style={styles.box}>
 
+        </View>
       </View>
-    </View>
-
+    </NavigationContainer>
   );
 }
 
