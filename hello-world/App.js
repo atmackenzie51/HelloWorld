2027a0c22e5a5ca52e99658b666f67c50a66768b
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-04-14 14:24:49 -0400

    adding more styles

--- hello-world/App.js
+++ hello-world/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.blue}>Hello World!</Text>
+      <Text style={styles.bigRed}>How are you?</Text>
+      <Text style={styles.bigRedBold}>I am feeling blue...</Text>
     </View>
   );
 }
@@ -15,6 +17,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  blue: {
+    color: 'blue',
+    fontWeight: '600',
+  },
+  bigRed: {
+    color: 'red',
+    fontSize: '30',
+  },
+  bigRedBold: {
+    color: 'red',
+    fontSize: '30',
+    fontWeight: '600',
   },
 });
 
