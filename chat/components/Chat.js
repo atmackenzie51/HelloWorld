8e2570ec2b420f8c7167dccfd2b01b52380fd1de
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-04-24 18:04:21 -0400

    creating messages useEffect

--- chat/components/Chat.js
+++ chat/components/Chat.js
@@ -1,11 +1,34 @@
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, background } = route.params;
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     navigation.setOptions({ title: name })
+  }, []);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
 
   return (
