a01f66eec5cf088e4b6897f08b52fb0a5e210279
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-05-05 13:16:56 -0400

    location and image upload functionality

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,11 +1,50 @@
-import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, View, StyleSheet, Alert } from "react-native";
 import { useActionSheet } from '@expo/react-native-action-sheet';
+//expo-location
 import * as Location from 'expo-location';
-import MapView from 'react-native-maps';
+//expo-image-picker
 import * as ImagePicker from 'expo-image-picker';
 
-const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage }) => {
+//imports from firebase
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+
+const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID }) => {
   const actionSheet = useActionSheet();
+
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+    if (permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      if (location) {
+        console.log(location)
+        onSend({
+          location: {
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude,
+          },
+        });
+      } else Alert.alert("Error occurred while fetching location");
+    } else Alert.alert("Permissions haven't been granted.");
+  };
+
+  const generateReference = (uri) => {
+    const timeStamp = (new Date()).getTime();
+    const imageName = uri.split("/")[uri.split("/").length - 1];
+    return `${userID}-${timeStamp}-${imageName}`;
+  };
+
+  const uploadAndSendImage = async (imageURI) => {
+    const uniqueRefString = generateReference(imageURI);
+    const newUploadRef = ref(storage, uniqueRefString);
+    const response = await fetch(imageURI);
+    const blob = await response.blob();
+    uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+      const imageURL = await getDownloadURL(snapshot.ref)
+      onSend({ image: imageURL })
+    });
+  }
+
   const onActionPress = () => {
     const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
     const cancelButtonIndex = options.length - 1;
@@ -29,6 +68,8 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage }) => {
       },
     );
   };
+
+
 
   return (
     <TouchableOpacity style={styles.container} onPress={onActionPress}>
